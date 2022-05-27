07c2e3eb2a3c122e8e798790e433026f1eff4604
cranetail <sam@gandalf.finance>
2022-05-27 20:06:12 +0800

    add swap

---
+++ contract/Awaken.Contracts.Swap/AwakenSwapContractReferenceState.cs
@@ -0,0 +1,17 @@
+using Awaken.Contracts.Token;
+
+namespace Awaken.Contracts.Swap
+{
+    public partial class AwakenSwapContractState
+    {
+        internal AElf.Contracts.MultiToken.TokenContractContainer.TokenContractReferenceState TokenContract
+        {
+            get;
+            set;
+        }
+
+        internal TokenContractContainer.TokenContractReferenceState LPTokenContract { get; set; }
+        
+        internal Awaken.Contracts.Investment.InvestmentContractContainer.InvestmentContractReferenceState VaultContract { get; set; }
+    }
+}
